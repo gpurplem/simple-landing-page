a26dc62b0062b694efb2d2238f763475707e41f9
gpurplem <megabrielbm@gmail.com>
2023-12-21 17:55:29 -0300

    footer remodel

--- footer.php
+++ footer.php
@@ -2,45 +2,46 @@
 
 <footer>
     <div class="container py-5">
-        <div class="row row-gap-5">
-            <div class="row-12 col-lg-1 justify-content-evenly align-self-sm-center d-flex flex-row flex-lg-column align-items-sm-center"
-                style=" gap: 35px; font-size: 2em;">
-                <a class="text-decoration-none" href="https://www.facebook.com">
+        <div class="footer-content--wrapper">
+
+            <div class='footer-social-icons--wrapper'>
+
+                <a href="https://www.facebook.com">
                     <i class="bi bi-facebook"></i>
                 </a>
-                <a class="text-decoration-none" href="https://www.instagram.com">
+
+                <a href="https://www.instagram.com">
                     <i class="bi bi-instagram"></i>
                 </a>
-                <a class="text-decoration-none" href="https://www.linkedin.com">
+
+                <a href="https://www.linkedin.com">
                     <i class="bi bi-linkedin"></i>
                 </a>
+
             </div>
 
-            <div class="row-12 col-lg-7 align-self-center text-center">
-                <span>Lorem Ipsum</span>
-                <hr style="width: 100%; background-color: white; height: 0.1vh; opacity: 1;">
-                <div class="d-flex" style="text-align: justify;">
+            <div class="footer-about--wrapper">
+                <h1>Lorem Ipsum</h1>
+                <hr>
+                <p>
                     Mauris ultrices finibus felis non porttitor. In lacus eros, tincidunt ut justo sit amet,
                     interdum dapibus dolor. Etiam et euismod metus. Fusce ullamcorper massa nisi, vitae feugiat elit
                     aliquam ac. Praesent volutpat euismod semper. Quisque eu lobortis magna, nec lacinia massa.
                     Aliquam at tortor sollicitudin, dictum ante vitae, ultricies massa. Praesent in dictum tortor.
-                </div>
+                </p>
             </div>
 
-            <div
-                class="row-12 col-lg-4 d-flex flex-row flex-lg-column align-self-lg-center justify-content-center align-items-center">
-                <div class="card"
-                    style="width: 18rem;background: none;border: none;align-items: center;text-align: center;">
-                    <i class="bi bi-person-check-fill" style="font-size: 2em;"></i>
-                    <div class="card-body">
-                        <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            <div class="footer-features--wrapper">
+                <div>
+                    <i class="bi bi-person-check-fill"></i>
+                    <div>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
                     </div>
                 </div>
-                <div class="card"
-                    style="width: 18rem;background: none;border: none;align-items: center;text-align: center;">
-                    <i class="bi bi-briefcase-fill" style="font-size: 2em;"></i>
-                    <div class="card-body">
-                        <p class="card-text">Ut id maximus mauris, ac molestie tellus</p>
+                <div>
+                    <i class="bi bi-briefcase-fill"></i>
+                    <div>
+                        <p>Ut id maximus mauris, ac molestie tellus</p>
                     </div>
                 </div>
             </div>
--- head.php
+++ head.php
@@ -1,9 +1,14 @@
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
 <link rel="stylesheet" href="./bootstrap-css/bootstrap.css">
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css">
 <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;500;600;700;800;900&display=swap"
-rel="stylesheet">
+    rel="stylesheet">
 
 <script src="./bootstrap-js/bootstrap.js"></script>
+
 <link rel="stylesheet" href="./styles.css">
+<link rel="stylesheet" href="./footer.css">
